8e96c389f09d6f32456d9305c37d8e2264581490
LimJH272 <limjiehan02@gmail.com>
2025-03-14 17:30:04 +0800

    Update helper functions

--- python_helpers.py
+++ python_helpers.py
@@ -3,6 +3,9 @@ import os
 def get_project_root_dir():
     return os.path.dirname(os.path.abspath(__file__))
 
+def get_main_data_dir():
+    return os.path.join(get_project_root_dir(), 'data')
+
 def print_with_indent(*to_print, n_indent: int, **kwargs):
     print('\t' * n_indent, end='')
     print(*to_print, **kwargs)
